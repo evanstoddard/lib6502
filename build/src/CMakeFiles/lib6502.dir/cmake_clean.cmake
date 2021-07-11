353c1c2412685ec5bdd74365c7cfebb6194e4240
Evan Stoddard <evanstoddard@Evans-MacBook-Pro.local>
2021-07-11 18:56:14 -0400

    Initial commit

---
+++ build/src/CMakeFiles/lib6502.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../lib/liblib6502.a"
+  "../lib/liblib6502.pdb"
+  "CMakeFiles/lib6502.dir/CPU_6502.c.o"
+  "CMakeFiles/lib6502.dir/CPU_6502.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lib6502.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/lib6502.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../lib/liblib6502.a"
+)
